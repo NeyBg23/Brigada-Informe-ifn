f2794fc4468accdcfcce33f72fc1f031431794d8
Carlos <martin5435412@gmail.com>
2025-10-08 16:59:05 -0500

    Update brigadas.js

--- src/routes/brigadas.js
+++ src/routes/brigadas.js
@@ -179,7 +179,7 @@ router.get("/hoja-vida/:nombreArchivo", async (req, res) => {
 
     if (error || !data) {
       console.error("❌ Error creando signed URL:", error);
-      return res.status(400).json({ error: "Error generando URL firmada" });
+      return res.status(400).json({ error: "Error generando URL firmada"+error.message });
     }
 
     res.json({ signedUrl: data.signedUrl });
