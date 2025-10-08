bb9a0c2f9f47ff2f7f08be9438d9f0e20e21f298
Carlos <martin5435412@gmail.com>
2025-10-08 16:05:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ app.use(cors({
 
 
 app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Ruta de prueba
 app.get("/", (req, res) => {
