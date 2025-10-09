88e48be0df3c79c68585a6752d7124a7d38fd237
Carlos <martin5435412@gmail.com>
2025-10-08 22:21:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -20,12 +20,10 @@ app.use(cors({
 
 
 app.use(express.json());
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Ruta de prueba
 app.get("/", (req, res) => {
-  res.json({ message: "Brigada Service funcionando 🚀"+process.env.SUPABASE_URL });
+  res.json({ message: "Brigada Service funcionando 🚀" });
 });
 
 // Rutas principales del microservicio
